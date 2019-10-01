2db4503068f3a29245807a1aa0c2a302f5ee3106
B533P-14\533 <olivier.allaire00@outlook.fr>
2019-10-01 11:15:40 -0400

    text fini

---
+++ TextUI/Assets/TextDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplay : MonoBehaviour {
+    public GameObject BangText;
+    public AudioSource audioSource;
+    float TimeAlive;
+    public Text displayText;
+
+    private bool hasClick;
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Fire1") != 0)
+        {
+
+            if (hasClick==false)
+            {
+       
+                Instantiate(BangText);
+                audioSource.Play();
+            }
+           
+
+            hasClick = true;
+        }
+        else
+        {
+
+            hasClick = false;
+        }
+	}
+}
---
+++ TextUI/Assets/destroytext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroytext : MonoBehaviour {
+    float timeAlive =0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeAlive +=Time.deltaTime;
+
+        if (timeAlive>=2)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
